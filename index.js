337aa13884704c288a0719927c91578de076f5cd
Jan Everaert <jan@crashlab.be>
2021-10-15 16:33:36 +0200

    feat: adding endpoint

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const messages = [];
 
 io.on('connection', client => {
   client.on('event', data => { 
+    messages.push(data);
 
    });
   client.on('disconnect', () => { /* … */ });
 });
 server.listen(3001);
 
+server.get("/list", (req, res) => {
+  res.send(messages);
+})
 
 server.listen(3000, () => {
   console.log("server listening at 3000")
